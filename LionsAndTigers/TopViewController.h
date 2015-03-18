01dc65c2c4fa9e13fc23d4952aef65415d10e0e6
Jazz-S <jazz1santiago@gmail.com>
2015-03-18 14:25:05 -0700

    added two containers and two buttons to HUDVC

---
+++ LionsAndTigers/TopViewController.h
@@ -0,0 +1,18 @@
+//
+//  TopViewController.h
+//  LionsAndTigers
+//
+//  Created by Jazz Santiago on 3/18/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol TopDelegate <NSObject>
+-(void)topRevealButtonTapped:(UIButton *)button;
+
+@end
+@interface TopViewController : UIViewController
+
+
+@end
